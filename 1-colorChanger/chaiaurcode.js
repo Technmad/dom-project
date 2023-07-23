3f5624f0ae5cab0aa6ccb4c3f07a6d2397c595fc
Technmad <111467507+Technmad@users.noreply.github.com>
2023-07-23 18:56:11 +0530

    project 1 done

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,12 @@
+const button = document.querySelectorAll('.button');
+// console.log(button);
+button.forEach(function (button) {
+  button.addEventListener('click', function (e) {
+    console.log(e.target.id);
+
+    if (e.target.id) {
+      const body = document.querySelector('body');
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
